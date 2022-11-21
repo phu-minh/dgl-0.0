29b9d01d7e10f7866a02b7573ff98ae65e679ecb
Nguyễn Minh Phú <19520218@gm.uit.edu.vn>
2022-11-21 13:03:37 +0700

    add comments

--- examples/pytorch/hilander/streamlit_app.py
+++ examples/pytorch/hilander/streamlit_app.py
@@ -9,3 +9,9 @@ st.write('This is a demo of Streamlit')
 df = pd.read_csv('result.csv')
 df_DBSCAN = pd.read_csv('resultDBSCAN.csv')
 
+
+
+st.write('This is a detail table of the result of the HILANDER algorithm')
+st.dataframe(df)
+st.write('This is a detail table of the result of the DBSCAN algorithm')
+st.dataframe(df_DBSCAN) 
